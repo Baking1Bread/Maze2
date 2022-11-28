33cb03211491c3c6ba4b34098fbeeaeb95c0a294
Paul Lötsch <paul.loetsch@fdtech.de>
2022-11-28 16:02:26 +0100

    Commentary

--- Maze/Grid.cpp
+++ Maze/Grid.cpp
@@ -3,35 +3,39 @@
 #include<vector>
 #include <iostream>
 #include<algorithm>
-#include<queue>
-#include<list>
-#include<stack>
 using namespace std;
 
 
 Labyrinth::Labyrinth()
 {	
-	int p = (N * M)/10;
+	//Variable int p spawns 10% Walls arround our Paths
+	int p = (N * M)/5;
+	//srand-time creates a Seed for our rand to get more than 1 Maze
 	srand(time(NULL));
+	//For-Loop to set everything to Paths
 	for (int x = 0; x < N; x++)
 	{
 		for (int y = 0; y < M; y++)
 		{
+			//1 = Path
 			array[x][y] = 1;
 		}
 	}
-	//Set Walls in Grid
+	//For-Loop to set random Walls/Obstacles
 	for (int i = 0; i < p; i++)
-	{
-		array[rand() % N][rand() % M] = 0;
+	{	
+		//0 = Wall
+		array[rand() % N][rand() % M] = 0; 
 	}
-
-	array[0][0] = 1; //Start-Point
-	array[N - 1][M - 1] = 1; //End-Point
+	//Start-Point set to Path, make sure there is no Wall
+	array[0][0] = 1; 
+	//End-Point set to Path, make sure there is no Wall
+	array[N - 1][M - 1] = 1; 
 }
 
 void Labyrinth::printArrayMaze()
-{
+{	
+	//For-Loop to Print our Maze
 	for (int x = 0; x < N; x++)
 	{
 		for (int y = 0; y < M; y++)
@@ -43,7 +47,8 @@ void Labyrinth::printArrayMaze()
 };
 
 void Labyrinth::printSolution()
-{
+{	
+	//For-Loop to print our Solution-Path
 	for (int i = 0; i < N; i++)
 	{
 		for (int j = 0; j < M; j++)
@@ -58,6 +63,7 @@ void Labyrinth::printSolution()
 bool Labyrinth::isSafe(int x, int y)
 {
 	//if (x,y) is outside of the maze return false
+	//This Method prevents our Solve Method to go out of Bounds
 	if (x >= 0 && x < N && y >= 0 && y < M && array[x][y] == 1)
 	{
 		return true;
@@ -66,9 +72,10 @@ bool Labyrinth::isSafe(int x, int y)
 }
 
 bool Labyrinth::solveMaze()
-{
-	int sol[N][M];
-	//memset(sol, 0, sizeof(sol));
+{	
+	//Solving Grid
+	//int sol[N][M];
+	//For Loop to set everthing on 0
 	for (int x = 0; x < N; x++)
 	{
 		for (int y = 0; y < M; y++)
@@ -76,206 +83,52 @@ bool Labyrinth::solveMaze()
 			sol[x][y] = 0;
 		}
 	}
+	//Printout Solution if solveMazeUtil(x,y) = goal, else "There is no Path to our goal"
 	if (solveMazeUtil(0, 0) == false)
 	{
-		cout << "There is no Solution";
+		cout << "There is no Path to our goal";
 		return false;
 	}
+	//Print Path
 	printSolution();
 	return true;
 }
 
 bool Labyrinth::solveMazeUtil(int x, int y)
 {
-	//Check for Goal
+	//Check for Goal, -1 because Arrays start at 0
 	if (x == N - 1 && y == M - 1 && array[x][y] == 1)
 	{
 		sol[x][y] = 1;
 		return true;
 	}
 
-
+	//Out-of-Bounds Method
 	if (isSafe(x, y) == true)
 	{
 		if (sol[x][y] == 1)
 		{
 			return false;
 		}
+		//Set sol[x][y] to 1 to mark it as a possible Path
 		sol[x][y] = 1;
+		//Check for the Right
 		if (solveMazeUtil(x + 1, y) == true)
 		{
 			return true;
 		}
+		//Check Down
 		if (solveMazeUtil(x, y + 1) == true)
 		{
 			return true;
 		}
+		//Unmark the Solutionpath if none of the movements worked
 		sol[x][y] = 0;
 		return false;
 	}
 	return false;
 }
 
-/*Labyrinth::Labyrinth(const unsigned int rows, const unsigned int columns, int fill)
-{
-
-	this->rows = rows;
-	this->columns = columns;
-
-	//Initialization Start x(first) & Start y(second)
-	Start.x = 0;
-	Start.y = 0;
-	End.x = columns-1;
-	End.y = rows-1;
-
-	for (int i = 0; i < rows; i++)
-	{
-		grid.push_back(vector<int>());
-
-		for (int j = 0; j < columns; j++)
-		{
-
-			int rnd = rand() % fill;
-			if (rnd == 1)
-			{
-				//1 = Wall
-				grid[i].push_back(1);
-			}
-			else
-			{
-				//0 = Path
-				grid[i].push_back(0);
-			}
-
-		}
-	}
-	grid[0][0] = 2; //Start-Punkt
-	grid[columns-1][rows-1]= 3;//End-Punkt
-}*/
-//----------------//
-//Check for Path  //
-//----------------//
-/*bool Labyrinth::isReachable()
-{
-	//Starting at 0,0
-	int i = 0, j = 0;
-	//Set Finish
-	fx = N - 1;
-	fy = M - 1;
-	//Set the visiting Array to true
-	memset(visited, true, sizeof(visited));
-
-	stack<Node> s;
-
-	Node temp(i, j);
-
-	s.push(temp);
-
-
-	while (!s.empty())
-	{
-		// Pop the top node and move to the
-		// left, right, top, down or retract
-		// back according the value of node's
-		// dir variable.
-		temp = s.top();
-		int d = temp.dir;
-
-		i = temp.x, j = temp.y;
-
-		temp.dir++;
-		//Remove Top Element
-		s.pop();
-		//Insert Element
-		s.push(temp);
-
-		//Check if we found the End
-		if (i == fx && j == fy)
-		{
-			return true;
-		}
-
-		//Checking UP
-		if (d == 0)
-		{
-			if (i - 1 >= 0 && array[i - 1][j] && visited[i - 1][j])
-			{
-				Node temp1(i - 1, j);
-				visited[i - 1][j] = false;
-				//Insert temp1 to Stack s
-				s.push(temp1);
-			}
-		}
-		//Checking Left
-		else if (d == 1)
-		{
-			if (j - 1 >= 0 && array[i][j - 1] && visited[i][j - 1])
-			{
-				Node temp1(i + 1, j);
-				visited[i + 1][j] = false;
-				//Insert temp1 to Stack s
-				s.push(temp1);
-			}
-		}
-		//Checking Down
-		else if (d == 2)
-		{
-			if (i + 1 < n && array[i + 1][j] && visited[i + 1][j])
-			{
-				Node temp1(i + 1, j);
-				visited[i + 1][j] = false;
-				//Insert temp1 to Stack s
-				s.push(temp1);
-			}
-		}
-		//Checking Right
-		else if (d == 3)
-		{
-			if (j + 1 < m && array[i][j + 1] && visited[i][j + 1])
-			{
-				Node temp1(i , j + 1);
-				visited[i][j + 1] = false;
-				//Insert temp1 to Stack s
-				s.push(temp1);
-			}
-		}
-		//Go back if no Path was found
-		else
-		{
-			visited[temp.x][temp.y] = true;
-			//cout << s.size()<<endl;
-			//Remove Top Element from Stack
-			s.pop();
-		}
-	}
-	return false;
-}*/
-/*void Labyrinth::printMaze()
-{	
-	for (int x = 0; x < rows; x++) 
-	{
-		for (int y = 0; y < columns; y++)
-		{	
-			if (Start.x == x && Start.y == y)
-			{
-				cout << "S";
-			}
-			else if (End.x == x && End.y == y)
-			{
-				cout << "E";
-			}
-			else if (grid[x][y] == 1)
-			{
-				cout << "x";
-			}
-			else
-			{
-				cout << " ";
-			}
-		}
-		cout << endl;
-	}
-}*/
 
 
 
--- Maze/Grid.h
+++ Maze/Grid.h
@@ -14,39 +14,17 @@ class Labyrinth
 {
 public:
 
-	Labyrinth();
-	void printArrayMaze();
-	void printSolution(); 
-	bool solveMaze();
-	bool isSafe(int x, int y);
-	bool solveMazeUtil(int x, int y);
-	int array[N][M]{ 0 };
-	int sol[N][M]{ 0 };
+	Labyrinth();						//Generates our Maze with Walls and Paths
+	void printArrayMaze();				//Prints our Maze
+	void printSolution();				//Print our Path
+	bool solveMaze();					//Combines SolveMazeUtil with printSolution.
+	bool isSafe(int x, int y);			//Check for Boundaries
+	bool solveMazeUtil(int x, int y);	//Recursive Method for (i+1,j) and (i,j+1)
+
+	int array[N][M]{ 0 };				//Declares 2D Integer Array->Maze
+	int sol[N][M]{ 0 };					//Declares 2D Integer Array->Path
 
 private:
-
-
-	//int n = N;
-	//int m = M;
-
-	//Defines End
-	//int fx = 0, fy = 0;
-
-
-	/*
-	struct Point
-	{
-		int x = 0;
-		int y = 0;
-	};
-	Point Start;
-	Point End;
-
-	unsigned int rows = 0;
-	unsigned int columns = 0;
-
-	std::vector<std::vector<int>>grid;
-	*/
 
 
 };
